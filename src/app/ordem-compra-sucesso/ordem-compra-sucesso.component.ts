9576b627515b8383912711ad5e8d49932b826baf
Bruno Guilherme Ferreira da Silva <bgfs@intranet.pitang.com>
2019-08-23 10:18:06 -0300

    Form - Ajuste no template Forms

--- src/app/ordem-compra-sucesso/ordem-compra-sucesso.component.ts
+++ src/app/ordem-compra-sucesso/ordem-compra-sucesso.component.ts
@@ -12,5 +12,4 @@ export class OrdemCompraSucessoComponent implements OnInit {
 
   ngOnInit() {
   }
-
 }
--- src/app/pedido-template-forms/pedido-template-forms.component.ts
+++ src/app/pedido-template-forms/pedido-template-forms.component.ts
@@ -2,7 +2,6 @@ import { Component, OnInit, ViewChild, Optional } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { OrdemCompraModel } from '../shared/ordem-compra.model';
 import { OrdemCompraService } from '../ordem-compra.service';
-
 @Component({
   selector: 'app-pedido-template-forms',
   templateUrl: './pedido-template-forms.component.html',
@@ -13,19 +12,30 @@ import { OrdemCompraService } from '../ordem-compra.service';
 })
 export class PedidoTemplateFormsComponent implements OnInit {
   public idPedido: number;
+  public submitted:boolean = false;
+
   constructor(private ordemCompraService: OrdemCompraService) { }
   @ViewChild('formPedido', {static: false}) public formPedido: NgForm;
-  ngOnInit() {
-  }
+  
+  ngOnInit() {}
+  onSubmit() { this.submitted = true}
+
   public realizarCompra(): void {
-    const formPedido: OrdemCompraModel = this.formPedido.form.value;
-    this.ordemCompraService.realizarCompra(formPedido).subscribe(
-      (res:OrdemCompraModel) => {
-        this.idPedido = res.id;
-      },
-      (error: Error) => {
-        console.log(error)
-      }
-    )
+    let formPedido: OrdemCompraModel = this.formPedido.form.value;
+    console.log(this.formPedido.form)
+    if(this.formPedido.form.valid) {
+      this.ordemCompraService.realizarCompra(formPedido).subscribe(
+        (res:OrdemCompraModel) => {
+          this.idPedido = res.id;
+        },
+        (error: Error) => {
+          console.log(error)
+        }
+      )
+    }
+    
+  }
+  public novoPedido() :void {
+    this.idPedido  = undefined;
   }
 } 
