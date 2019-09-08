bf3d6cf757e8499267b02d70408b64fe9e3bbb15
BrunoGuilherme14 <brunoguilhermefs@gmail.com>
2019-09-08 17:36:03 -0300

    Ajustando ambiente de prd para heroku

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const app = express();
  
 app.use(express.static('./dist/passaro-urbano'));
  
-app.get('/*', (req, res) => {
+app.get('*', (req, res) => {
     res.sendFile(path.join('./dist/passaro-urbano/index.html'));
 });
  
